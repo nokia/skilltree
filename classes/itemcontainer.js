b6dd500fa44b56f1f5d996b4c147ec4fdaff7f13
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-09-25 17:57:19 +0200

    Added skill levels, click functions to change levels.

--- classes/itemcontainer.js
+++ classes/itemcontainer.js
@@ -1,57 +1,97 @@
 export class ItemContainer {
-    constructor(position_x, position_y, skillicon, skillborder) {
+    constructor(position_x, position_y, skillicon, skillborder, levelborder, skill_level, max_skill_level) {
         //Loading images
-        this.skillicon = new PIXI.Sprite.fromImage(skillicon);
-        this.skillborder = new PIXI.Sprite.fromImage(skillborder);
+        this.skillicon = new PIXI.Sprite.fromImage(skillicon); //100x100
+        this.skillborder = new PIXI.Sprite.fromImage(skillborder); //116x116
+        this.levelborder = new PIXI.Sprite.fromImage(levelborder);
+
+        //Setting border variables
+        this.skillborder.skill_level = skill_level;
+        this.skillborder.max_skill_level = max_skill_level;
+        this.skillborder.levelinfo = new PIXI.Text(skill_level + " / "+ max_skill_level);
 
         //Initilaizing container
         this.container = new PIXI.Container();
         this.container.addChild(this.skillicon);
+        this.container.addChild(this.levelborder);
         this.container.addChild(this.skillborder);
+        this.container.addChild(this.skillborder.levelinfo);
+        
 
+        //Setting size, position of objects in container
         this.skillicon.anchor.set(0.5, 0.5);
         this.skillborder.anchor.set(0.5, 0.5);
+        this.levelborder.anchor.set(1,0);
+        this.skillborder.levelinfo.anchor.set(1,0);
 
-        this.skillicon.position.set(65, 65);
-        this.skillborder.position.set(65, 65);
+        this.skillicon.position.set(58, 58);
+        this.skillborder.position.set(58, 58);
+        this.levelborder.position.set(115,1);
+        this.skillborder.levelinfo.position.set(105, 15);
+        this.skillborder.levelinfo.scale.set(0.5);
 
+        //Adding events to the container (to border, covers it all)
         this.skillborder.interactive = true;
         this.skillborder
-            .on("pointerdown", this.onButtonDown)
+            .on('click', this.onClick)
             .on('pointerover', this.onButtonOver)
             .on('pointerout', this.onButtonOut)
-            .on('rightdown', this.onRightDown);
+            .on('rightclick', this.onRightClick);
 
         //Moving container to its spot.
         this.container.position.set(position_x, position_y);
 
+        
+        
+
     }
 
-    onButtonDown()
+    onClick()
     {
-        this.isdown = true;
-        this.filters = [new PIXI.filters.GlowFilter(6,2,2, 0xFF4000, 1)];
+        
+        if(this.skill_level<this.max_skill_level)
+        {
+            this.skill_level ++;
+            this.levelinfo.text = (this.skill_level + " / " + this.max_skill_level);
+            console.log(this.skill_level);
+            if(this.skill_level==this.max_skill_level)
+            {
+                this.isdown = true;
+                this.filters = [new PIXI.filters.GlowFilter(6,2,2, 0xFF4000, 1)];
+            }
+        }
+        
+        
+        
+        
     }
 
     onButtonOver()
     {
-        this.isOver = true;
-        if(this.isdown) return;
+        if(this.skill_level == this.max_skill_level) return;
         this.filters = [new PIXI.filters.GlowFilter(6,2,2, 0xFFBF00, 1)];
     }
 
     onButtonOut()
     {
-        this.isOver = false;
-        if(this.isdown) return;
+        if(this.skill_level == this.max_skill_level) return;
         this.filters = null;
     }
 
-    onRightDown()
+    onRightClick()
     {
-        
-        this.filters = [new PIXI.filters.GlowFilter(6,2,2, 0xFFFFFF, 1)];
+        if(this.skill_level>0)
+        {
+            this.skill_level --;
+            this.levelinfo.text = (this.skill_level + " / " + this.max_skill_level);
+        } else return;
+
+            this.filters = [new PIXI.filters.GlowFilter(6,2,2, 0xFFBF00, 1)];
     }
 
+    addHoverContainer(hovercontainer) //Sets hover containers position to the itemcontainer
+    {
+        //TODO
+    }
 
 }
--- main.js
+++ main.js
@@ -11,15 +11,21 @@ var data = [
         position_y: 100,
         skillicon: "pictures/skillicon.jpg",
         skillborder: "pictures/skillborder.png",
+        levelborder: "pictures/levelborder.jpg",
+        skill_level: 2,
+        max_skill_level: 5
     },
     {
         position_x: 100,
         position_y: 400,
         skillicon: "pictures/skillicon.jpg",
         skillborder: "pictures/skillborder.png",
+        levelborder: "pictures/levelborder.jpg",
+        skill_level: 0,
+        max_skill_level: 5
     },
 ];
 for (var i = 0; i < data.length; ++i) {
-    var itemcontainer = new ItemContainer(data[i].position_x, data[i].position_y, data[i].skillicon, data[i].skillborder);
+    var itemcontainer = new ItemContainer(data[i].position_x, data[i].position_y, data[i].skillicon, data[i].skillborder, data[i].levelborder, data[i].skill_level, data[i].max_skill_level);
     app.stage.addChild(itemcontainer.container);
 }
