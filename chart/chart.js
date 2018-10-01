cf4d75d293e0cbfa366932e85992abc40f0c9de0
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-10-01 16:23:07 +0200

    Added slices

--- chart/chart.js
+++ chart/chart.js
@@ -1,11 +1,17 @@
-var app = new PIXI.Application(
-    {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        backgroundColor: 0x000000,
-        antialias: true,
-        autoStart: true, // TODO false and rendering only when needed
-    }
-);
+var app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    antialias: true,
+    transparent: true,
+});
 
 document.body.appendChild(app.view);
+
+var sliceNum = 8;
+for (var i = 0; i < sliceNum; ++i) {
+    var slice = new PIXI.Graphics();
+    slice.lineStyle(20, 0x000000);
+    slice.arc(window.innerWidth / 2, window.innerHeight / 2, 20, i * Math.PI * 2 / sliceNum + Math.PI / 60, (i + 1) * Math.PI * 2 / sliceNum - Math.PI / 60);
+
+    app.stage.addChild(slice);
+}
