3cdc3fc433723e4591ef75eb49760881eb2073e9
benisoliver <oliver@benis.eu>
2018-10-04 11:11:38 +0200

    snowflake chart

--- chart/chart.js
+++ chart/chart.js
@@ -7,43 +7,42 @@ var app = new PIXI.Application({
 
 document.body.appendChild(app.view);
 
-app.stage.interactive = true;
-
-var slices = new Array();
-
-var levelNum = 5;
+/*var levelNum = 5;
 var sliceNum = 8;
+var slices = new Array(sliceNum);
 var k = 0;
-for (var i = 0; i < levelNum; ++i) {
-    //var level = new Array();
-    for (var j = 0; j < sliceNum; ++j) {
-        slices[k] = new PIXI.Graphics();
-        slices[k].lineStyle(30, 0x000000);
-        slices[k].arc(window.innerWidth / 2, window.innerHeight / 2, 32 * i + 60, j * Math.PI * 2 / sliceNum + Math.PI * 2 / (200 + i * 80), (j + 1) * Math.PI * 2 / sliceNum - + Math.PI * 2 / (200 + i * 80));
-        slices[k].interactive = true;
-        slices[k].hitArea = slices[k].getBounds();
+for (var i = 0; i < 1; ++i) {
+    for (var j = 3; j < 4; ++j) {
+        if (i == 0) slices[j] = new Array(levelNum);
 
-        slices[k].mouseover = function (event) {
-            event.currentTarget.filters = [new PIXI.filters.GlowFilter(6, 2, 2, 0xFF0000, 1)];
-        };
+        var sliceG = new PIXI.Graphics();
+        sliceG.lineStyle(30, 0x000000);
+        sliceG.arc(window.innerWidth / 2, window.innerHeight / 2, 32 * i + 60, j * Math.PI * 2 / sliceNum + Math.PI * 2 / (200 + i * 80), (j + 1) * Math.PI * 2 / sliceNum - Math.PI * 2 / (200 + i * 80));
 
-        slices[k].mouseout = function (event) {
-            event.currentTarget.filters = null;
-        };
+        var sliceT = sliceG.generateTexture();
 
-        /*slice.on(`pointerover`, function () {
-            slice = new PIXI.Graphics();
-            slice.lineStyle(30, 0xff0000);
-            slice.arc(window.innerWidth / 2, window.innerHeight / 2, 32 * i + 60, j * Math.PI * 2 / sliceNum + Math.PI * 2 / 110, (j + 1) * Math.PI * 2 / sliceNum - + Math.PI * 2 / 110);
-        });*/
+        var sliceGClick = new PIXI.Graphics();
+        sliceG.lineStyle(30, 0xff0000);
+        sliceG.arc(window.innerWidth / 2, window.innerHeight / 2, 32 * i + 60, j * Math.PI * 2 / sliceNum + Math.PI * 2 / (200 + i * 80), (j + 1) * Math.PI * 2 / sliceNum - Math.PI * 2 / (200 + i * 80));
 
-        app.stage.addChild(slices[k]);
-        //slices.push(slice);
+        var sliceTClick = sliceGClick.generateTexture();
 
+        slices[j][i] = new PIXI.Sprite(sliceT);
+
+        //slice.anchor.x = - bounds.x / sliceT.width; ????????????????????????????????????????????
+        slices[j][i].position.set(sliceG.getBounds().x, sliceG.getBounds().y);
+        slices[j][i].interactive = true;
+        slices[j][i].buttonMode = true;
+
+        slices[j][i].on(`pointerdown`, function () {
+            this.texture = sliceTClick;
+        });
+
+        app.stage.addChild(slices[j][i]);
         ++k;
     }
     //slices.push(level);
-}
+}*/
 
 /*for (var j = 0; j < sliceNum; ++j) {
     var divider = new PIXI.Graphics();
---
+++ snowflake/_next/08f7bbd1-48df-4c27-afc4-f28bce2365c9/page/_document/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_document', function() {
+            var comp = module.exports=webpackJsonp([4],[],[225]);
+            return { page: comp.default }
+          })
+        
