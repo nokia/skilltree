c2aedb821aa14f28613769999ba153e870cafb72
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-10-08 13:04:21 +0200

    deleted demo
    It is on the demo branch.

--- snowflake/_next/08f7bbd1-48df-4c27-afc4-f28bce2365c9/page/_document/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/_document', function() {
-            var comp = module.exports=webpackJsonp([4],[],[225]);
-            return { page: comp.default }
-          })
-        
