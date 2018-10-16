a60e362148697c7be7548ecbeb10d1895d53c621
benisoliver <oliver@benis.eu>
2018-10-16 10:05:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const path = require('path');
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = 80
+const app = express();
+const port = 80;
 
-app.use(express.static('./'))
-app.get('/', (req, res) => res.sendFile('login.html', { root: path.join(__dirname, './') }))
+// serving static files and opening login.html
+app.use(express.static('./'));
+app.get('/', (req, res) => res.sendFile('login.html', { root: path.join(__dirname, './') }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.listen(port);
