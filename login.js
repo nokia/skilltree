fc6d1f51dcf84c08666e0b10bafd923860da1552
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-10-16 14:16:51 +0200

    Update login.js

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ function validate() {
 	var loginData = {user: {email: username.value, password: password.value}};
 
     var loginRequest = new XMLHttpRequest();
-    loginRequest.open("POST", '/', true);
+    loginRequest.open("POST", '/api/users', true);
 
     loginRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
