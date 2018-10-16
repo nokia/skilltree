faba6024cdf1c647d13752aef31838bc73f5b526
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-10-16 12:53:04 +0200

    Added authentication

--- app.js
+++ app.js
@@ -1,13 +1,75 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
 
+
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
 const app = express();
+
+
+// Port
 const port = 80;
 
 // serving static files and opening login.html
 app.use(express.static('./'));
 app.get('/', (req, res) => res.sendFile('login.html', { root: path.join(__dirname, './') }));
 
+//app.listen(port);
+// -----------------------------
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'sugarfree', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
+if(!isProduction) {
+  app.use(errorHandler());
+}
+
+
+require('./models/Users');
+
+
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/skilltree');
+mongoose.set('debug', true);
+
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
 
 app.listen(port);
+
+
