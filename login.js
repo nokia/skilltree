0961d5b7c9d17073915cd2dfd5074385897543e6
benisoliver <oliver@benis.eu>
2018-10-16 15:27:48 +0200

    Revert "Update login.js"
    This reverts commit c21ef034e105dd98a3dba51485dd3a124cd448e6.

--- login.js
+++ login.js
@@ -19,7 +19,7 @@ function validate() {
             console.log(resp.token);
         }
     }
-    loginRequest.send(loginData);
+    loginData.send(loginData);
 
     /*if(username.value == "test" && password.value == "TEST") {
 		loginBox.style.display = "none";
