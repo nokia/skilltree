dd866fc154f1a1ba34d335ec71ab1f59860aac60
benisoliver <oliver@benis.eu>
2018-10-16 10:02:41 +0200

    nodejs express webserver

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-/*const http = require('http');
-var url  = require('url');
-var fs = require('fs');
-
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile(request.url, null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-}
-
-http.createServer(onRequest).listen(3000);*/
-
+const path = require('path');
 const express = require('express')
+
 const app = express()
 const port = 80
 
 app.use(express.static('./'))
-app.get('/', (req, res) => res.send('./login.html'))
+app.get('/', (req, res) => res.sendFile('login.html', { root: path.join(__dirname, './') }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
