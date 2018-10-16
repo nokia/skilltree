c39cc814c0f17b052905d2a4ef5e441412e2b650
benisoliver <oliver@benis.eu>
2018-10-16 09:46:59 +0200

    .

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.use(express.static('./'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
