60d83f053415e8453716a901a5d57d2706c97b5c
benisoliver <oliver@benis.eu>
2018-10-17 10:30:32 +0200

    Update usermodel.js

--- usermodel.js
+++ usermodel.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({
-    name: String,
+    username: String,
     hashData: String
 }));
