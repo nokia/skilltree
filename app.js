dfff4c133925f2608c21a8fc788c9a64372cb684
benisoliver <oliver@benis.eu>
2018-10-17 11:16:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/registration', function(req, res) {
     var hashData = pbkdf2.hashPassword(req.body.password);
 
     console.log('sdsd');
-    console.log(hashData);
+    console.log(hashData.toString());
 
     var newUser = new User({
         username: req.body.username,
