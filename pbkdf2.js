c3c624e201c5d588095ba94d201ae506c4329afb
benisoliver <oliver@benis.eu>
2018-10-17 11:54:55 +0200

    Update pbkdf2.js

--- pbkdf2.js
+++ pbkdf2.js
@@ -1,40 +1,41 @@
 var crypto = require('crypto');
 
-async function hashPassword (password) {
+function hashPassword (password) {
     const hashLength = 32; // in bytes
     const saltLength = 16; // in bytes
     const iterations = 248573;
 
     var hashData = undefined;
+    var salt = undefined;
 
-    await crypto.randomBytes(saltLength, function(err, salt) { // generates salt (only this line)
+    crypto.randomBytes(saltLength, function(err, saltt) { // generates salt (only this line)
         if (err) {
             return err;
         }
-
-        crypto.pbkdf2Sync(password, salt, iterations, hashLength,
-            function(err, hash) {
-
-                if (err) {
-                    //return err;
-                }
-
-                hashData = new Buffer(hash.length + salt.length + 8);
-
-                // include the size of the salt so that we can, during verification,
-                // figure out how much of the hash is salt
-                hashData.writeUInt32BE(salt.length, 0, true);
-                // similarly, include the iteration count
-                hashData.writeUInt32BE(iterations, 4, true);
-
-                salt.copy(hashData, 8);
-                hash.copy(hashData, salt.length + 8);
-
-                //return hashData;
-            });
-
-        console.log(hashData);
+        
+        salt = saltt;
     });
+
+    crypto.pbkdf2Sync(password, salt, iterations, hashLength,
+        function(err, hash) {
+
+            if (err) {
+                //return err;
+            }
+
+            hashData = new Buffer(hash.length + salt.length + 8);
+
+            // include the size of the salt so that we can, during verification,
+            // figure out how much of the hash is salt
+            hashData.writeUInt32BE(salt.length, 0, true);
+            // similarly, include the iteration count
+            hashData.writeUInt32BE(iterations, 4, true);
+
+            salt.copy(hashData, 8);
+            hash.copy(hashData, salt.length + 8);
+
+            //return hashData;
+        });
 
     return hashData;
 }
