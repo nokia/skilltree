1ff366a70cb22e01601b68691a56bdd708e7ef00
batixsz <batixsz@gmail.com>
2018-10-17 13:06:26 +0200

    registration, deleting useless stuff

--- login/register.js
+++ login/register.js
@@ -1,7 +1,9 @@
 function validate() {
 	var loginBox = document.getElementById("loginBox");
 	var username = document.getElementById("username");
-	var password = document.getElementById("password");
+	var password1 = document.getElementById("password1");
+	var password2 = document.getElementById("password2");
+	var email = document.getElementById("email");
 
 	if(true) {
 		loginBox.style.display = "none";
