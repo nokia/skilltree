ac0490ffce3727c860f2caff99697918e240a819
batixsz <batixsz@gmail.com>
2018-10-18 15:53:07 +0200

    Update protected/chartandtree.js

--- protected/chartandtree.js
+++ protected/chartandtree.js
@@ -13,7 +13,7 @@ var app = new PIXI.Application(
 
 function logout(){
   setCookie("loginToken", "", 1);
-  window.open("/login", "_self");
+  window.open("./login", "_self");
 }
 
 function setCookie(cname, cvalue) {
