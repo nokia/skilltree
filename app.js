e6326a543060817c625214c20e7001f7b445811f
benisoliver <oliver@benis.eu>
2018-10-29 09:36:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -205,7 +205,7 @@ setRoute.post('/skilllevel', function(req, res) {
                     treeID: data[i].treeID,
                     skills: []
                 });
-                if (user.skillData.find(obj => obj.treeID).skills.find(obj => obj.skillID == data[i].skillID) == undefined) user.skillData.find(obj => obj.treeID).skills.push({skillID: data[i].skillID});
+                if (user.skillData.find(obj => obj.treeID == ).skills.find(obj => obj.skillID == data[i].skillID) == undefined) user.skillData.find(obj => obj.treeID).skills.push({skillID: data[i].skillID});
 
                 user.skillData.find(obj => obj.treeID).skills.find(obj => obj.skillID == data[i].skillID).skillLevel = data[i].skillLevel;
 
