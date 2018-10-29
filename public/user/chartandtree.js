3f57f9bc3230410832ec214a7b2badec4927c62e
benisoliver <oliver@benis.eu>
2018-10-29 13:19:23 +0100

    Update public/user/chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -10,7 +10,7 @@ userDataRequest.responseType = "json";
 userDataRequest.onreadystatechange = function() {
     if(userDataRequest.readyState == 4 && userDataRequest.status == 200) {
         userData = userDataRequest.response;
-        console.log(userData);
+        console.log(userData.skillData);
         if (userData.skillData == undefined) window.open("/user/!!!!VALAMI!!!!!", "_self"); // userhez tree hozzaado
         else initChart();
     }
