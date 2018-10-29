038eee8263152f2e7dbb469c405ade16516163e7
benisoliver <oliver@benis.eu>
2018-10-29 10:56:13 +0100

    Update public/user/classes/itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -2,6 +2,7 @@ class ItemContainer {
     constructor(app, treeData, userData, treeID, skillID) {
         this.app = app;
         this.treeData = treeData; // only this tree's data
+        this.userData = userData;
         this.skillData = treeData.skills[skillID];
         this.skillData.treeID = treeID;
         this.skillData.skillLevel = this.getSkillLevel(userData, skillID);
