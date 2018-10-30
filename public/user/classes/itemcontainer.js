b7e07fafcf65f2aff895f710903787389fa5a7bb
benisoliver <oliver@benis.eu>
2018-10-30 11:16:09 +0100

    Update public/user/classes/itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -337,36 +337,6 @@ class ItemContainer {
         this.app.renderer.render(this.app.stage);
     }
 
-    // TODO Can it be in separate class???
-    createLink (textString, url, style, underline) {
-        var link = new PIXI.Container();
-
-        var text = new PIXI.Text(textString, style);
-        link.addChild(text);
-
-        if (underline && style !== undefined && style.wordWrap !== true) {
-            var line = new PIXI.Graphics();
-            line.lineStyle(text.height / 10, style.fill);
-            line.moveTo(0, text.height * 11 / 12);
-            line.lineTo(text.width, text.height * 11 / 12);
-            link.addChild(line);
-        }
-
-        var buttonRect = new PIXI.Graphics();
-        buttonRect.drawRect(0, 0, text.width, text.height);
-        var button = new PIXI.Sprite(buttonRect.generateTexture());
-        button.interactive = true;
-        button.buttonMode = true;
-        button.on("pointerdown", function () {
-            window.open(url, '_self');
-        });
-        link.addChild(button);
-
-        this.button = button;
-
-        return link;
-    }
-
     getSkillLevel (userData, skillID) {
         if (userData != undefined) {
             if (userData.skills.find(obj => obj.skillID == skillID) != undefined) return userData.skills.find(obj => obj.skillID == skillID).skillLevel;
