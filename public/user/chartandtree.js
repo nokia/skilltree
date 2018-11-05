03f006c6bb812a7ad7ae0833a2930ab6d4828522
benisoliver <oliver@benis.eu>
2018-11-05 15:18:55 +0100

    Update public/user/chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -218,7 +218,7 @@ function initChart() {
         //sliceContainer[i].title = rope;
 
         app.stage.addChild(sliceContainer[i]);
-        console.log(sliceContainer[i]);
+        console.log(sliceContainer[i].width);
     }
 
     logo = new PIXI.Sprite(PIXI.loader.resources["tree.png"].texture);
