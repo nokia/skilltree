d43cd5598f0f57cd34588c1189640eb4993ff8c9
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-11-06 14:42:20 +0100

    Update public/login.js

--- public/login.js
+++ public/login.js
@@ -14,6 +14,9 @@ function validate() {
 			if(httpRequest.readyState == 4 && httpRequest.status == 200) {
 				if(httpRequest.response.success){
 					localStorage.setItem("loginToken", httpRequest.response.token);
+					httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+					httpRequest.setRequestHeader('x-access-token', localStorage.getItem("loginToken"));
+					
 					window.open("/user/", "_self");
 				} else {
                     showToast();
