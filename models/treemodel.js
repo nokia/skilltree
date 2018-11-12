92ea095e26bb2e5959f0b9683768d5969366e285
batixsz <batixsz@gmail.com>
2018-11-12 13:12:16 +0100

    Update models/treemodel.js

--- models/treemodel.js
+++ models/treemodel.js
@@ -2,6 +2,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var ObjectID = mongoose.Schema.Types.ObjectId;
+
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('Tree', new Schema({
     _id: ObjectID,
