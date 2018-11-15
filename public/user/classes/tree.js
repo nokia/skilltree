8f30f225a44aed1bca65b2f343f092fcee9f2f3b
batixsz <batixsz@gmail.com>
2018-11-15 12:22:44 +0100

    Update public/user/classes/tree.js

--- public/user/classes/tree.js
+++ public/user/classes/tree.js
@@ -22,8 +22,9 @@ class Tree {
         var levelLength = [];
 
         for(var i = 0; i < this.skills.length; i++){
+          console.log(tmpChildren.find(obj => obj.name == this.skills[i].name);
           if(tmpChildren.find(obj => obj.name == this.skills[i].name) !== undefined){
-            levelLength[level] = n;
+            levelLength.push(n); //levelLength[level] = n;
             level += 1;
             tmpChildren = [];
             n = 0;
