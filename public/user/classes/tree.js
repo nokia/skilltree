9cbf84544e12d9dcac92cb92e998c16cdb1c1270
benisoliver <oliver@benis.eu>
2018-11-19 15:56:03 +0100

    Update tree.js

--- public/user/classes/tree.js
+++ public/user/classes/tree.js
@@ -40,6 +40,8 @@ class Tree {
         for(var i = 0; i < this.skills.length; i++){
           this.skills[i].itemcontainer = new ItemContainer(app, this.skills, this.skills[i].name);
 
+          console.log(this.skills[i].place);
+          console.log(levelLength[this.skills[i].level]);
           this.skills[i].itemcontainer.container.position.x = this.skills[i].place * 130 + (app.renderer.width - levelLength[this.skills[i].level] * 130) / 2;
           console.log(this.skills[i].itemcontainer.container.position.x);
           this.skills[i].itemcontainer.container.position.y = this.skills[i].level * 150;
