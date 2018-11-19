6fdbc5a54dd6a990a21fa8c8eb6a693c10e2b27c
batixsz <batixsz@gmail.com>
2018-11-19 13:49:00 +0100

    Update itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -202,7 +202,7 @@ class ItemContainer {
         for (var j = 0; j < this.skills[i].parents.length; j++) {
           var par = this.skills.find(obj => obj.name == this.skills[i].parents[j]);
           if(par !== undefined){
-            if(par.children.find(obj => obj.name == this.skills[i].name).minPoint > par.achievedPoint || par.itemcontainer.container.interactive = false){
+            if(par.children.find(obj => obj.name == this.skills[i].name).minPoint > par.achievedPoint || par.itemcontainer.container.interactive == false){
               var colorMatrixFilter = new PIXI.filters.ColorMatrixFilter;
               colorMatrixFilter.brightness(0.4);
               this.skills[i].itemcontainer.container.filters = [colorMatrixFilter];
