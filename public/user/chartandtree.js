3c3331c7d52c980cdf16c5e9075d7324f817ab79
benisoliver <oliver@benis.eu>
2018-11-19 17:19:07 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -91,8 +91,26 @@ function submit(){
         window.open("/user/", "_self");
       }
   }
-  console.log(data.skills);
-  sub.send(JSON.stringify(data.skills));
+  var cache = [];
+var jsonData = JSON.stringify(data.skills, function(key, value) {
+    if (typeof value === 'object' && value !== null) {
+        if (cache.indexOf(value) !== -1) {
+            // Duplicate reference found
+            try {
+                // If this value does not reference a parent it can be deduped
+                return JSON.parse(JSON.stringify(value));
+            } catch (error) {
+                // discard key if value cannot be deduped
+                return;
+            }
+        }
+        // Store value in our collection
+        cache.push(value);
+    }
+    return value;
+});
+cache = null;
+  sub.send(jsonData);
 }
 
 function logout(){
