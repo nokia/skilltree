714b02dfea0f46f487f0b5598d6565b83e8b22d1
benisoliver <oliver@benis.eu>
2018-11-20 14:11:11 +0100

    Update register.js

--- public/register.js
+++ public/register.js
@@ -9,36 +9,32 @@ function validate() {
 	if (password1.value == password2.value) {
 		if (validateEmail(email.value)) {
 			if (checkPassword(password1.value)) {
-		var httpRequest = new XMLHttpRequest();
-		httpRequest.open('POST', '/registration', true);
-		httpRequest.setRequestHeader('Content-type', 'application/json');
-		httpRequest.responseType = "json";
+				var httpRequest = new XMLHttpRequest();
+				httpRequest.open('POST', '/registration', true);
+				httpRequest.setRequestHeader('Content-type', 'application/json');
+				httpRequest.responseType = "json";
 
-		//Listener, if response comes, it runs.
-		httpRequest.onreadystatechange = function() {
-		    if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-					if (httpRequest.response.success) {
-						localStorage.setItem("loginToken", httpRequest.response.token);
-						window.open("/user", "_self");
+				//Listener, if response comes, it runs.
+				httpRequest.onreadystatechange = function() {
+		    		if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+						if (httpRequest.response.success) {
+							localStorage.setItem("loginToken", httpRequest.response.token);
+							window.open("/user", "_self");
+						} else showToast();
 					}
-					else showToast();
-			}
+				}
 
-		}
-		httpRequest.send(
-			JSON.stringify({
-				username: username.value,
-				password: password1.value,
-				email: email.value,
-				focusArea: focusarea.value
-			})
-		);
-	} else alert("The password is not valid! It has to contain at least one digit, one lowercase and one uppercase character. The minimum password length is 8 characters.");
+				httpRequest.send(
+					JSON.stringify({
+						username: username.value,
+						password: password1.value,
+						email: email.value,
+						focusArea: focusarea.value
+					})
+				);
+			} else alert("The password is not valid! It has to contain at least one digit, one lowercase and one uppercase character. The minimum password length is 8 characters.");
 		} else alert("The email address is not valid!");
-	}
-	else {
-		alert("Incorrect credentials! Passwords don't match!");
-	}
+	} else alert("Incorrect credentials! Passwords don't match!");
 }
 
 function validateEmail (email) {
