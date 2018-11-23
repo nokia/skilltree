170fb36cf29cd3dd3612d67f669858497f22ee5b
benisoliver <oliver@benis.eu>
2018-11-23 11:12:20 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -194,8 +194,11 @@ function showChart() {
         var tempContainer = new PIXI.Container();
 
         var skills = data.skills.filter(obj => obj.categoryName == data.categories[i].name);
+        console.log(skills);
         var sumAP = skills.reduce((a, b) => a.achievedPoint + b.achievedPoint, 0);
         var sumMP = skills.reduce((a, b) => a.maxPoint + b.maxPoint, 0);
+        console.log(sumAP);
+        console.log(sumMP);
         var percent = 0;
         if (sumMP != 0) percent = sumAP / sumMP;
 
