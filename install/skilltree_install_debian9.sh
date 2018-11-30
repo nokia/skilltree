eea669debbb972b9372e5400ec70823190b910dd
Sicambria <inczegaspar@gmail.com>
2018-11-30 18:16:27 +0100

    Update skilltree_install_debian9.sh

--- install/skilltree_install_debian9.sh
+++ install/skilltree_install_debian9.sh
@@ -5,19 +5,23 @@
 # DO NOT INCLUDE WWW IN THE DOMAIN NAME
 DOMAIN=yourdomain.org
 
-# Install sources
+# Add install sources
 sudo apt install curl software-properties-common git -y
-curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 # MongoDB
 curl https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
-sudo touch /etc/apt/sources.list.d/mongodb-org-4.0.list
-sudo echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" > /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo sh -c "echo 'deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main' >> /etc/apt/sources.list.d/mongodb-org-4.0.list"
+# NodeJS
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 
 sudo apt update
 
+# Install MongoDB
 sudo apt install mongodb-org -y
 sudo systemctl enable mongod
 sudo systemctl start mongod
+
+# Install NodeJS
+sudo apt-get install -y nodejs
 
 cd
 cd skilltree
@@ -55,17 +59,17 @@ sudo pm2 startup systemd
 if [ $1 = "Apache" ]
 then
   echo "Installing Apache"
-  sudo apt install python-certbot-apache -y
+  sudo apt install apache2 python-certbot-apache -y
   # sudo nano /etc/apache2/sites-available/$DOMAIN.conf
-  sudo echo "ServerName $DOMAIN;" > /etc/apache2/sites-available/$DOMAIN.conf
+  sudo sh -c "echo 'ServerName $DOMAIN;' >> /etc/apache2/sites-available/$DOMAIN.conf"
   sudo apache2ctl configtest
   sudo certbot --apache -d $DOMAIN -d www.$DOMAIN
   sudo a2enmod proxy
   sudo a2enmod proxy_http
 else
   # sudo nano /etc/apt/sources.list
-  sudo echo "deb http://deb.debian.org/debian stretch-backports main contrib non-free" > /etc/apt/sources.list
-  sudo echo "deb-src http://deb.debian.org/debian stretch-backports main contrib non-free" > /etc/apt/sources.list
+  sudo sh -c "echo 'deb http://deb.debian.org/debian stretch-backports main contrib non-free' >> /etc/apt/sources.list"
+  sudo sh -c "echo 'deb-src http://deb.debian.org/debian stretch-backports main contrib non-free' >> /etc/apt/sources.list"
   sudo apt update
   sudo apt install python-certbot-nginx -y -t stretch-backports
   sudo certbot --nginx -d $DOMAIN -d $DOMAIN
@@ -73,7 +77,5 @@ fi
 
 chmod 755 /etc/letsencrypt/live/
 chmod 755 /etc/letsencrypt/archive/
-
-
 
 # https://www.digitalocean.com/community/tutorials/how-to-set-up-a-node-js-application-for-production-on-debian-9
