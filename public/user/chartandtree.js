2bc0b20d45fc1834c22cec49ab1c5bf0062591a6
benisoliver <oliver@benis.eu>
2018-11-30 11:37:12 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -574,7 +574,7 @@ function create() {
         for (var i = 0; i < data.categories.length; ++i) {
             var option = document.createElement("option");
             option.text = data.categories[i].name;
-            skillList.add(option);
+            catSelect.add(option);
         }
     };
 
