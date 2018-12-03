ba95121c154c80bc7fbceff34550d0e00e8b7451
benisoliver <oliver@benis.eu>
2018-12-03 13:18:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ setRoute.post('/searchSkillsByName', async function (req, res) { // should searc
         });
 
         user = user.toObject();
-        var foundUserSkills = user.skills.filter(obj => obj.name.match(new RegExp(".*b.*", "i")) != null);
+        var foundUserSkills = user.skills.filter(obj => obj.name.match(new RegExp(".*a.*", "i")) != null);
         console.log(foundUserSkills);
 
         var foundGlobalSkills = await Skill.find({
