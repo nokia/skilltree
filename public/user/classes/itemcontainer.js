f74e6bff83a222f55e5e90c764ca82fefb50f081
benisoliver <oliver@benis.eu>
2018-12-03 18:13:34 +0100

    Update itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -11,7 +11,7 @@ class ItemContainer {
 
         //Setting border variables
         this.skillborder.levelinfo = new PIXI.Text(this.skill.achievedPoint + "/" + this.skill.maxPoint, {fontSize: 12});
-        this.skillborder.levelinfo.resolution = 10;
+        this.skillborder.levelinfo.resolution = 5;
 
         //Creating details page
         var detailsWidth = 240;
