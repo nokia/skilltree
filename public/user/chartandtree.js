c85f93695b6fb310b00b3e46797f67282f9ff933
batixsz <batixsz@gmail.com>
2018-12-03 17:24:49 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -1,18 +1,22 @@
 var data = undefined;
 
-// get data from server
-var dataRequest = new XMLHttpRequest();
-dataRequest.open('GET', '/get/userdata', true);
-dataRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-dataRequest.setRequestHeader('x-access-token', localStorage.getItem("loginToken"));
-dataRequest.responseType = "json";
-dataRequest.onreadystatechange = function() {
-    if(dataRequest.readyState == 4 && dataRequest.status == 200) {
-        data = dataRequest.response;
-        checkFirstLogin();
-    }
+initData();
+
+function initData(){
+  var dataRequest = new XMLHttpRequest();
+  dataRequest.open('GET', '/get/userdata', true);
+  dataRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  dataRequest.setRequestHeader('x-access-token', localStorage.getItem("loginToken"));
+  dataRequest.responseType = "json";
+  dataRequest.onreadystatechange = function() {
+      if(dataRequest.readyState == 4 && dataRequest.status == 200) {
+          data = dataRequest.response;
+          checkFirstLogin();
+      }
+  }
+  dataRequest.send();
 }
-dataRequest.send();
+// get data from server
 
 var app = new PIXI.Application({
         view: pixiCanvas,
