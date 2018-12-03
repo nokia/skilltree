b5d9a63a03bc94b530ad663c8e0f4a7329c79fd8
benisoliver <oliver@benis.eu>
2018-12-03 17:37:56 +0100

    Update itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -152,8 +152,6 @@ class ItemContainer {
         this.container
             .on('pointerover', this.onButtonOver)
             .on('pointerout', this.onButtonOut);
-
-        this.container.scale.set(.7);
     }
 
 
