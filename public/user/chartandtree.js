73d76cc2acc69397c91c652d05feb68bebae9968
batixsz <batixsz@gmail.com>
2018-12-04 10:51:50 +0100

    Revert "Update chartandtree.js"
    This reverts commit 17b74944b139bb337a269a20d03c0fc257f69cce.

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -221,9 +221,7 @@ function startLoader () {
 }
 
 app.stage = new PIXI.display.Stage();
-var bgimage = PIXI.Sprite.fromImage('pictures/bg.jpg');
-bgimage.zOrder = 1000;
-app.stage.addChild(bgimage);
+app.stage.addChild(PIXI.Sprite.fromImage('pictures/bg.jpg'));
 app.stage.group.enableSort = true;
 
 // CHART
