ecb0671ebd33184d47ac4fee37ddae2815dad090
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-12-07 15:49:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -940,17 +940,19 @@ setRoute.post('/dropoffers', async function (req, res) {
 //API call for request onclick
 setRoute.post('/request', async function (req, res){
 
-	var user = User.findOne({username: req.decoded.username}, function(err, user) {
+	var user = await User.findOne({username: req.decoded.username}, function(err, user) {
 		if (err) throw err;
+		console.log('user find done');
 		return user;
 	});
 
-	var skill = Skill.findOne({name: req.body.name},  function (err, skill) {
+	var skill = await Skill.findOne({name: req.body.name},  function (err, skill) {
 		if (err) throw err;
+		console.log('skill find done');
 		return skill;
 	});
 
-	if (skill != undefined) {
+	if (skill !== undefined) {
 		var userskill = user.skills.find(obj => obj.name == skill.name);
 
 		if(skill.requests.find(obj => obj.username == user.username) == undefined)
