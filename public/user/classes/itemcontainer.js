13e58ff375e5ffe28d33ccc05d2ae036c0ecf217
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-12-10 14:16:08 +0100

    Update itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -408,7 +408,7 @@ class ItemContainer {
                                         {
                                             alert(requestforrequests.response.message);
                                             console.log(requestforrequests.response);
-                                            beginnerCount.innerHTML = (requestforrequests.response.sumRequest);
+                                            intermediateCount.innerHTML = (requestforrequests.response.sumRequest);
 
                                         }
                                     }
