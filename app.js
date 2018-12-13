6ce90d7a85028928d65409115381be9237e12743
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2018-12-13 18:31:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -495,7 +495,7 @@ setRoute.post('/approveskill', async function (req, res)  {
 	console.log(globalskill);
 
 	//Check if skill is already in the database or not
-	if(globalskill !== undefined)
+	if(globalskill != [] )
 		{
 			res.json({
 				success: false,
