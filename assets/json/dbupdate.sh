cd8bf7edca66a487c1de7d0091758fd83755b077
Sicambria <inczegaspar@gmail.com>
2018-12-15 16:25:06 +0100

    delete current data before import

--- assets/json/dbupdate.sh
+++ assets/json/dbupdate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Drop current skills, trees and categories collections & update from JSON?"
+read
+echo -e "use skilltreenew'\n'db.trees.drop()'\n'db.skills.drop()'\n'db.categories.drop()" > dropcollections.js
+mongo < dropcollections.js
+
 mongoimport --db skilltreenew --collection skills --file skills.json --jsonArray
 mongoimport --db skilltreenew --collection trees --file trees.json --jsonArray
 mongoimport --db skilltreenew --collection categories --file categories.json --jsonArray
