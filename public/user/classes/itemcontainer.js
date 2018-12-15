0a593991b63b625d06842efe041dea7731298170
Oliver Benis <oliver@benis.hu>
2018-12-15 18:41:26 +0100

    .

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -84,7 +84,7 @@ class ItemContainer {
                 req.responseType = "json";
                 req.onreadystatechange = function () {
                     if(this.readyState == 4 && this.status == 200) {
-                        console.log(this.parentObj);
+                        console.log("nnn");
                     }
                 };
 
