66d4d97d67fb894b1439ef87c7fb94f0c816e938
benisoliver <oliver@benis.eu>
2018-12-17 09:07:12 +0100

    Update trainingsforapprovemodel.js

--- models/trainingsforapprovemodel.js
+++ models/trainingsforapprovemodel.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('ApprovableTraining', new Schema({
-    username: String
+    username: String,
     skillName: String,
     name: String,
     level: Number,
