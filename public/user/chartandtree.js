4cc499a2de930138134d5e67790b5b9cc5000032
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2019-01-04 16:18:28 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -805,7 +805,7 @@ function editMySkill () {
                     var a = 0;
                 }
                 
-                request('POST', '/set/searchUserSkillByNames', [{ name: "HTML" }], function(){
+                request('POST', '/set/parentTableData', [{ name: "HTML" }], function(){
                     if (this.readyState == 4 && this.status == 200) {
                         if(this.response !== undefined)
                         {
