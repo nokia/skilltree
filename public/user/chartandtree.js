f657081b209ac25bb08a2534b11011c385cd355f
benisoliver <oliver@benis.eu>
2019-01-09 09:02:59 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -1276,7 +1276,8 @@ function editMyTree () {
 
 function getChildren (skills, skill, children) {
 	var temp = [];
-    console.log("ss");
+    console.log(skills);
+    console.log(skill);
 	for (var i = 0; skill.children != undefined && i < skill.children.length; ++i) {
         var child = skills.find(obj => obj.name == skill.children[i].name);
 
