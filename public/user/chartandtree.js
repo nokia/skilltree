1b0a59c8c06f55e09eb5d82a8bcdd46ff5891ba6
benisoliver <oliver@benis.eu>
2019-01-09 10:32:15 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -213,7 +213,7 @@ function searchTreesByName (element, global) {
         });
     } else {
         var res = data.trees.filter(obj => obj.name.match(new RegExp("/.*" + treeToSearch + ".*/i")) != null);
-        console.log((/.*web.*/i)).test("Web Developer"));
+        console.log((/.*web.*/i).test("Web Developer"));
         for (var i = 0; i < res.length; ++i) {
             var mya = document.createElement('option');
             mya.value = res[i].name;
