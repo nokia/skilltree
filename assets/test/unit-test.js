d5c3401e13c412e727d6bdd0a4cabcab57548daa
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2019-01-10 12:21:07 +0100

    .

--- assets/test/unit-test.js
+++ assets/test/unit-test.js
@@ -15,13 +15,17 @@ describe("hashPassword() and verifyPassword()", function(){
 
 describe("asd", function(){
     it("should do smth", function(){
+        var result = undefined;
+
         request('GET', '/apitest', undefined , function () {
             if (this.readyState == 4 && this.status == 200) {
-                expect(this.response.succes).to.equal(true);
+                result = this.response.succes;
+
+                expect(result).to.equal(false);
             }
         });
 
-
+        
 
         
     });
@@ -40,7 +44,7 @@ describe("asd", function(){
 
 function request (type, url, sendData, callback) {
     var req = new XMLHttpRequest();
-    req.open(type, url, true);
+    req.open(type, "https://skilltree.benis.hu/" + url, true);
     req.setRequestHeader('Content-type', 'application/json');
     req.setRequestHeader('x-access-token', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhdHJpazEiLCJhZG1pbiI6InRydWUiLCJpYXQiOjE1MTYyMzkwMjJ9.DpR8IB4Ir3YLI7nfcpHRf3L64lEcmv2ixnSh8H1xVaI");
     req.responseType = "json";
