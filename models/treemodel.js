69a7fa03d095bb9624c3417b575f37719daf15c7
benisoliver <oliver@benis.eu>
2019-01-14 11:18:54 +0100

    Update treemodel.js

--- models/treemodel.js
+++ models/treemodel.js
@@ -5,6 +5,7 @@ var Schema = mongoose.Schema;
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('Tree', new Schema({
     name: String,
+    description: String,
     skillNames: [String],
     focusArea: String
 }));
