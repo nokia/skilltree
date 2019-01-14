9723eae3116d9508f9904337d05b5a5bccf0b31e
benisoliver <oliver@benis.eu>
2019-01-14 13:37:08 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -358,6 +358,11 @@ function editMySkill () {
                 document.getElementById('newSkillIcon').value = this.response.skillIcon;
                 document.getElementById("newSkillCat").value = this.response.categoryName;
 
+                var pointsTable = document.getElementById('pointsTable');
+
+                for (var i = pointsTable.rows.length-1; i > 1; --i) pointsTable.deleteRow(i);
+
+                trainingsTable.rows[1].cells[0].children[0].value = "";
 
 
                 /*//Dropping data from parentsTable
@@ -375,11 +380,12 @@ function editMySkill () {
                 //Dropping data from trainingsTable
                 var trainingsTable = document.getElementById('trainingsTable');
                 var i=trainingsTable.rows.length-1;
-                while(i>1)
-                {
+
+                while(i>1) {
                     trainingsTable.deleteRow(i);
                     i--;
                 }
+
                 trainingsTable.rows[1].cells[0].children[0].value = "";
                 trainingsTable.rows[1].cells[1].children[0].value = "";
                 trainingsTable.rows[1].cells[2].children[0].value = "";
@@ -391,13 +397,13 @@ function editMySkill () {
                 for (var i = 0; i < this.response.trainings.length; ++i) {
                     addRow("trainingsTable");
 
-                    trainingsTable.rows[i+1].cells[0].children[0].value = this.response.trainings[i].name;
-                    trainingsTable.rows[i+1].cells[1].children[0].value = this.response.trainings[i].level;
-                    trainingsTable.rows[i+1].cells[2].children[0].value = this.response.trainings[i].shortDescription;
-                    trainingsTable.rows[i+1].cells[3].children[0].value = this.response.trainings[i].URL;
-                    trainingsTable.rows[i+1].cells[4].children[0].value = this.response.trainings[i].goal;
-                    trainingsTable.rows[i+1].cells[5].children[0].value = this.response.trainings[i].length;
-                    trainingsTable.rows[i+1].cells[6].children[0].value = this.response.trainings[i].language;
+                    trainingsTable.rows[i + 1].cells[0].children[0].value = this.response.trainings[i].name;
+                    trainingsTable.rows[i + 1].cells[1].children[0].value = this.response.trainings[i].level;
+                    trainingsTable.rows[i + 1].cells[2].children[0].value = this.response.trainings[i].shortDescription;
+                    trainingsTable.rows[i + 1].cells[3].children[0].value = this.response.trainings[i].URL;
+                    trainingsTable.rows[i + 1].cells[4].children[0].value = this.response.trainings[i].goal;
+                    trainingsTable.rows[i + 1].cells[5].children[0].value = this.response.trainings[i].length;
+                    trainingsTable.rows[i + 1].cells[6].children[0].value = this.response.trainings[i].language;
                 }
 
 
