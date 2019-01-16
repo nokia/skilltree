6eb72b20694b96dafb548b21956369f4b88b31e4
benisoliver <oliver@benis.eu>
2019-01-16 12:57:45 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -779,6 +779,7 @@ function addSkillToList (skillsToAdd) {
     request('POST', '/protected/getskill', skill, function() {
         if(this.readyState == 4 && this.status == 200) {
             if (this.response.success) {
+                console.log(skillsToAdd);
                 if (skillsToAdd.find(obj => obj.name == this.response.skill.name) == undefined) {
                     if (this.response.dependency.length > 0) {
                         var text = "The selected skill depends on the following skills. Do you want to add these?\n";
