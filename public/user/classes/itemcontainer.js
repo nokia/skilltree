5b34689bcb550454b944bd158bc9126f6b8710c1
batixsz <batixsz@gmail.com>
2019-01-17 14:06:44 +0100

    Update itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -365,7 +365,7 @@ class ItemContainer {
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
 
         if (this.parentObj.skill.achievedPoint == this.parentObj.skill.maxPoint) return;
-        this.parentObj.setFilter(this.parentObj, hoverFilter, this.parentObj.filters[1]);
+        this.parentObj.setFilter(this.parentObj, hoverFilter, this.parentObj.skills.filters[1]);
 
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
     }
@@ -382,7 +382,7 @@ class ItemContainer {
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
 
         if (this.parentObj.skill.achievedPoint == this.parentObj.skill.maxPoint || this.parentObj.disabled) return;
-        this.parentObj.setFilter(this.parentObj, nullFilter, this.parentObj.filters[1]);
+        this.parentObj.setFilter(this.parentObj, nullFilter, this.parentObj.skills.filters[1]);
 
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
     }
