0c83efb07c2855f6282f07981ee6ca6bcacc9bac
batixsz <batixsz@gmail.com>
2019-01-17 10:39:52 +0100

    Update itemcontainer.js

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -341,7 +341,7 @@ class ItemContainer {
                         this.skills[i].itemcontainer.skillborder.interactive = false;
                         this.skills[i].itemcontainer.skillborder.buttonMode = false;
 
-                        this.parentObj.setFilter(this.skills[i]);
+                        this.setFilter(this.skills[i]);
                     }
                     else {
                         this.skills[i].itemcontainer.skillborder.filters = null;
@@ -352,7 +352,7 @@ class ItemContainer {
                         this.skills[i].itemcontainer.skillborder.interactive = true;
                         this.skills[i].itemcontainer.skillborder.buttonMode = true;
 
-                        this.parentObj.setFilter(this.skills[i]);
+                        this.setFilter(this.skills[i]);
                     }
                 }
             }
