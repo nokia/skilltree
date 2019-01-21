4f08c54e2fd39d532815d5ae4b9c603340e91c7f
benisoliver <oliver@benis.eu>
2019-01-21 13:28:38 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -1112,7 +1112,7 @@ function hideMenus () {
 }
 
 function hideCards (event) {
-    var except = "#userCard, .float-right *";
+    var except = "#newSkillModal *, #userCard, .float-right *";
 
     if (!event.target.matches(except)) {
         $(".collapse").collapse("hide");
