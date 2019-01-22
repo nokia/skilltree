b26a5520526d04b5f7b94a98de1b1c68468cbe63
batixsz <batixsz@gmail.com>
2019-01-22 15:15:14 +0100

    Revert "Update app.js"
    This reverts commit eba3c188d83bbba721da4f06a89a81f887132bc1.

--- app.js
+++ app.js
@@ -528,7 +528,6 @@ async function assembleTree(skillMatrix){
 		l = false;
 		for (var component = 0; component < skillMatrix.length; component++) {
 			assembledTree.push(...skillMatrix[component][j]);
-			assembleTree = assembleTree.concat(skillMatrix[component][j]);
 			console.log("comp:");
 			console.log(skillMatrix[component][j]);
 			l = true;
