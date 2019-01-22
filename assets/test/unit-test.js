dd6a82a8718c0bd2def9028ccb0431644c65de75
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2019-01-22 14:24:22 +0100

    Update unit-test.js

--- assets/test/unit-test.js
+++ assets/test/unit-test.js
@@ -118,7 +118,7 @@ describe("Deleting user", function(){
              request.post({ 
                  url: baseUrl + "/admin/deleteUser", 
                  headers: { 'x-access-token': adminToken }, 
-                 body: {username: 'testuser'}
+                 body: JSON.stringify({username: 'testuser'})
                 },
                 function(error, response, body) {
                     console.log(error, body);
