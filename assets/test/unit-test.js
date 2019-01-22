0bb1dc8267d04e46d467fd5ba195f64bdb7385e0
dubstepz321 <36195977+dubstepz321@users.noreply.github.com>
2019-01-22 14:18:54 +0100

    Update unit-test.js

--- assets/test/unit-test.js
+++ assets/test/unit-test.js
@@ -85,6 +85,7 @@ describe("Deleting user", function(){
     it("Should delete", function(done){
         //this.timeout(10000);
 
+        /*
         var bodyData = {
             username: 'testuser'
         }
@@ -110,20 +111,21 @@ describe("Deleting user", function(){
 
             done();
         });
+*/
 
 
-
-             /*
+             
              request.post({ 
                  url: baseUrl + "/admin/testAdmin", 
-                 headers: { 'x-access-token': adminToken, 'Content-type': 'application/json' }, 
-                 body: JSON.stringify({username: 'testuser'})
+                 json: true,
+                 headers: { 'x-access-token': adminToken}, 
+                 body: {username: 'testuser'}
                 },
                 function(error, response, body) {
                     console.log(error, body);
                     //expect(JSON.parse(body).success).to.equal(true);
                     done();
-            });*/
+            });
     });
 });
 
