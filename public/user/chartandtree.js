0457f1e7f239e62245fbe7da2136bc76a5acca3a
benisoliver <oliver@benis.eu>
2019-01-22 11:45:02 +0100

    Update chartandtree.js

--- public/user/chartandtree.js
+++ public/user/chartandtree.js
@@ -201,7 +201,7 @@ function addTraining () {
 // opens skill creation modal and saves the new skill to the database (user and for approval)
 function createSkill () {
     //$('#newSkillForm').reset();
-    $('#newSkillForm').data('bootstrapValidator').resetForm();
+    $('#newSkillForm').resetForm();
 
     var modal = document.getElementById("newSkillModal");
     modal.style.display = "block";
