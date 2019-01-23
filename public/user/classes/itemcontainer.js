8eeaffa1fd14c801879a0e23d7199b7b8cc2395c
benisoliver <oliver@benis.eu>
2019-01-23 10:06:33 +0100

    .

--- public/user/classes/itemcontainer.js
+++ public/user/classes/itemcontainer.js
@@ -357,7 +357,9 @@ class ItemContainer {
 
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
 
+        console.log(this.parentObj.disabled);
         if (this.parentObj.skill.achievedPoint == this.parentObj.skill.maxPoint  || this.parentObj.disabled) return;
+        console.log("ww");
         this.parentObj.setFilter(this.parentObj, hoverFilter, this.parentObj.skillborder.filters[1]);
 
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
@@ -375,6 +377,7 @@ class ItemContainer {
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
 
         if (this.parentObj.skill.achievedPoint == this.parentObj.skill.maxPoint || this.parentObj.disabled) return;
+
         this.parentObj.setFilter(this.parentObj, nullFilter, this.parentObj.skillborder.filters[1]);
 
         this.parentObj.app.renderer.render(this.parentObj.app.stage);
--- public/user/src/init.js
+++ public/user/src/init.js
@@ -60,6 +60,7 @@ function checkFirstLogin() {
         var btn = document.getElementById('savebtn');
         var mainTree = document.getElementById('maintree');
 
+        console.log("b");
         btn.onclick = function() {
             var location = document.getElementById('location').value;
             var teachingDay = document.getElementById('day').value;
@@ -72,9 +73,6 @@ function checkFirstLogin() {
                     teachingTime: teachingTime,
                     location: location
             };
-
-            console.log("s");
-            console.log(event);
 
             /*request('POST', '/protected/firstlogindata', firstLoginData, function() {
                 if(this.readyState == 4 && this.status == 200) {
@@ -130,12 +128,12 @@ function loadAddedTrees(){
 }
 
 window.addEventListener('load', function() {
+    console.log("a");
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
         form.addEventListener('submit', function(event) {
-            console.log(event);
             if (form.checkValidity() === false) {
                 event.preventDefault();
                 event.stopPropagation();
