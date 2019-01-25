d6f1b63279291a8127636d25ff84ceab4c97ce44
batixsz <batixsz@gmail.com>
2019-01-25 16:46:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -379,6 +379,8 @@ protectedRoute.post('/getPublicSkillData', async function (req, res) {
 			}
 			foundSkills[s].users = outUsers;
 		}
+		console.log(foundSkills[0]);
+		console.log(foundSkills[0].users);
     res.json(foundSkills);
 });
 
