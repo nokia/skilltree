c3bec354a7e1be97f18f7a9215078cbee69a171e
batixsz <batixsz@gmail.com>
2019-01-25 16:11:09 +0100

    debug commit

--- app.js
+++ app.js
@@ -378,7 +378,7 @@ protectedRoute.post('/getPublicSkillData', async function (req, res) {
 			}
 			foundSkills[s].users = outUsers;
 		}
-		console.log(foundSkills[0]);
+		console.log(foundSkills[0].users);
     res.json(foundSkills);
 });
 
