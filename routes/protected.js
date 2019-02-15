cc77ce87d08cb6d00e83c7ab87b15a62d0ab7164
benisoliver <oliver@benis.eu>
2019-02-15 11:05:01 +0100

    Update protected.js

--- routes/protected.js
+++ routes/protected.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose');
+const jwt    = require('jsonwebtoken');
 
 module.exports = function (app) {
     var protectedRoute = express.Router();
