e06097313cbaab5f6f45d9d0a4a83aa815854c80
benisoliver <oliver@benis.eu>
2019-02-18 14:53:44 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 
 module.exports = function (app){
     fs.readdirSync(__dirname).forEach(function(file) {
+        console.log(file);
         if (file == "index.js") return;
         var name = file.substr(0, file.indexOf('.'));
         require('./' + name)(app);
