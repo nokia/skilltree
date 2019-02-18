e2ca3ec0083c83054626bf45349c7f2eb641387c
benisoliver <oliver@benis.eu>
2019-02-18 15:03:33 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -481,4 +481,45 @@ module.exports = function (app) {
         skillArray = await extractNames(sortedArray);
         return skillArray;
     }
+
+    async function insertSkill(skillToInsert, skillMatrix) {
+        for (var component = 0; component < skillMatrix.length; component++) {
+            for (var child = 0; child < skillToInsert.children.length; child++) {
+                for (var row = 0; row < skillMatrix[component].length; row++) {
+                    if ((skillMatrix[component][row].map(obj => obj.name)).includes(skillToInsert.children[child].name)) {
+                        if (row == 0) {
+                            await addRowToComponent(skillMatrix, component);
+                            skillMatrix[component][0].push(skillToInsert);
+                            // console.log(skillToInsert.name + " added to root(skills child found in tree)"); // for debugging reasons
+                            // this checks if the row found was the root level.
+                            // if yes, it adds another row to the top, and inserts the skill there.
+                            return;
+                        }
+                        else {
+                            skillMatrix[component][row - 1].push(skillToInsert);
+                            // console.log(skillToInsert.name + " added to the row above(skills child found in tree)"); // for debugging reasons
+                            // if no, it inserts the skill to the row above.
+                            return;
+                        }
+                    }
+                }
+            }
+            for (var par = 0; par < skillToInsert.parents.length; par++) {
+                for (var row = 0; row < skillMatrix[component].length; row++) {
+                    if ((skillMatrix[component][row].map(obj => obj.name)).includes(skillToInsert.parents[par])) {
+                        if (skillMatrix[component][row + 1] == undefined) skillMatrix[component].push([]);
+                        skillMatrix[component][row + 1].push(skillToInsert);
+                        // console.log(skillToInsert.name + " added to the row below(skills parent found in tree.)"); // for debugging reasons
+                        // this checks if the skill has any parents in any row in any component,
+                        // if yes, then it inserts the skill to the row below.
+                        return;
+                    }
+                }
+            }
+        }
+        skillMatrix.push([[skillToInsert]]);
+        // console.log(skillToInsert.name + " added to a new component."); // for debugging reasons
+        // this inits the first element of every component
+        return;
+    }
 }
